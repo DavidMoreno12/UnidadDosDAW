c2161c18be51ebae466f7b03ba1401bf962fa1ec
DavidMoreno12 <jdavidmh_29@hotmail.com>
2017-02-18 12:05:59 -0600

    creation mypins and add user at pins table

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :pins
   get 'welcome/index'
   root 'welcome#index'
-
+  get 'mypins' => 'pins#mypins'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
